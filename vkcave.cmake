b4161f415e53981431574f098d6f925493cc7289
Trevor Hedstrom <trevorhedstrom@gmail.com>
2019-10-11 17:34:25 -0700

    unix

--- vkcave.cmake
+++ vkcave.cmake
@@ -1,4 +1,10 @@
-set(CMAKE_CXX_STANDARD 17)
+if (CMAKE_VERSION VERSION_LESS "3.1")
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+		set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+	endif ()
+else ()
+	set (CMAKE_CXX_STANDARD 17)
+endif ()
 
 # Check for dependencies
 if(DEFINED ENV{VULKAN_SDK})
@@ -7,35 +13,35 @@ else()
     message(FATAL_ERROR "Error: VULKAN_SDK not set!")
 endif()
 
-if(DEFINED ENV{GLFW_HOME})
-    message(STATUS "Found GLFW_HOME")
-else()
-    message(FATAL_ERROR "Error: GLFW_HOME not set!")
-	# https://www.glfw.org/download.html
-endif()
-
-if(DEFINED ENV{ASSIMP_HOME})
-    message(STATUS "Found ASSIMP_HOME")
-else()
-    message(FATAL_ERROR "Error: ASSIMP_HOME not set!")
-	# http://www.assimp.org/index.php/downloads
-endif()
-
-if(DEFINED ENV{SPIRV_CROSS_HOME})
-    message(STATUS "Found SPIRV_CROSS_HOME")
-else()
-    message(FATAL_ERROR "Error: SPIRV_CROSS_HOME not set!")
-	# https://github.com/KhronosGroup/SPIRV-Cross
-endif()
-
 # Include dependencies
-include_directories(
-	"$ENV{VULKAN_SDK}/include"
-	"$ENV{SPIRV_CROSS_HOME}/include"
-	"$ENV{GLFW_HOME}/include"
-	"$ENV{ASSIMP_HOME}/include" )
+include_directories("$ENV{VULKAN_SDK}/include")
 
 if(WIN32)
+	if(DEFINED ENV{GLFW_HOME})
+		message(STATUS "Found GLFW_HOME")
+	else()
+		message(FATAL_ERROR "Error: GLFW_HOME not set!")
+		# https://www.glfw.org/download.html
+	endif()
+
+	if(DEFINED ENV{ASSIMP_HOME})
+		message(STATUS "Found ASSIMP_HOME")
+	else()
+		message(FATAL_ERROR "Error: ASSIMP_HOME not set!")
+		# http://www.assimp.org/index.php/downloads
+	endif()
+
+	if(DEFINED ENV{SPIRV_CROSS_HOME})
+		message(STATUS "Found SPIRV_CROSS_HOME")
+	else()
+		message(FATAL_ERROR "Error: SPIRV_CROSS_HOME not set!")
+		# https://github.com/KhronosGroup/SPIRV-Cross
+	endif()
+
+	include_directories(
+		"$ENV{SPIRV_CROSS_HOME}/include"
+		"$ENV{GLFW_HOME}/include"
+		"$ENV{ASSIMP_HOME}/include" )
 	add_definitions(-DWINDOWS -DWIN32_LEAN_AND_MEAN -DNOMINMAX)
 
 	# Link vulkan and assimp
@@ -58,6 +64,17 @@ if(WIN32)
 	endif()
 
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd26812 /wd26451") # unscoped enum, arithmetic overflow
+elseif (UNIX)
+	# Link vulkan, assimp, and GLFW
+	link_libraries(
+		"$ENV{VULKAN_SDK}/lib/libvulkan.so"
+		"libassimp.so.3"
+		"libglfw.so.3" )
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		link_libraries("$ENV{VULKAN_SDK}/lib/libVkLayer_utils.a")
+	endif()
+else()
+	message(FATAL_ERROR "Error: Not implemented!")
 endif()
 
 # GLFW defines
@@ -67,7 +84,7 @@ function(link_plugin TARGET_NAME)
 	if(WIN32)
 		target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/lib/Engine.lib")
 	elseif(UNIX)
-		message(FATAL_ERROR "Error: Not implemented!")
+		target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/lib/Engine.a")
 	else()
 		message(FATAL_ERROR "Error: Not implemented!")
 	endif(WIN32)
