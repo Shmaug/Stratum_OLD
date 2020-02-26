41236a6dbd19f2f406c0096c6732523174edbf4f
Trevor Hedstrom <trevorhedstrom@gmail.com>
2020-02-26 09:16:45 -0800

    yeet

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ ASSIMP_DIR=$(pwd)/ThirdParty/assimp
 SHADERC_DIR=$(pwd)/ThirdParty/shaderc
 SPIRV_CROSS_DIR=$(pwd)/ThirdParty/shaderc/third_party/spirv-cross
 
+
+echo Installing dependencies...
+yum install vulkan-devel zlib-devel libX11-devel libXrandr-devel
+echo Dependencies installed.
+
 echo Updating submodules...
 git submodule update --init
 echo Submodules updated.
@@ -14,27 +19,24 @@ cd $ASSIMP_DIR
 cmake CMakeLists.txt -S "$ASSIMP_DIR" -B "$ASSIMP_DIR" -Wno-dev -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ZLIB=ON -DINJECT_DEBUG_POSTFIX=OFF -DLIBRARY_SUFFIX="" -DCMAKE_INSTALL_PREFIX="$ASSIMP_DIR"
 echo Assimp configured.
 echo Building Assimp...
-cmake --build . --config Release --target install
+make install -j16
 echo Assimp built.
 
 
 cd $SHADERC_DIR
-python utils/git-sync-deps
+python3 utils/git-sync-deps
 
 echo Configuring Shaderc...
 cmake CMakeLists.txt -S "$SHADERC_DIR" -B "$SHADERC_DIR" -Wno-dev -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DSHADERC_SKIP_TESTS=ON -DSPIRV_SKIP_EXECUTABLES=ON -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="$SHADERC_DIR"
 echo Shaderc configured.
 echo Building Shaderc...
 cmake --build . --config Release --target add-copyright
-cmake --build . --config Release --target install
+make install -j16
 echo Shaderc built.
 
-echo Configuring SPIRV-cross...
-cd $SPIRV_CROSS_DIR
-cmake CMakeLists.txt -S "$SPIRV_CROSS_DIR" -B "$SPIRV_CROSS_DIR" -Wno-dev -DCMAKE_BUILD_TYPE=Release -DSPIRV_CROSS_SHARED=OFF -DSPIRV_CROSS_STATIC=ON -DSPIRV_CROSS_ENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX="$SPIRV_CROSS_DIR" 
-echo SPIRV-cross configured.
 echo Building SPIRV-cross...
-cmake --build . --config Release --target install
+cd $SPIRV_CROSS_DIR
+make -j16
 echo SPIRV-cross built.
 
-cd %STRATUM_DIR%
+cd $STRATUM_DIR
